78dda0b910adc555f3169441d84e185c7bf7aed1
Jspsun <Jspsun@gmail.com>
2017-08-28 00:31:10 -0400

    finished base NavBar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Jonathan.Solar - My Personal website
 
-# [Seeing it speaks better than any readme can explain: Jonathan.Solar](http://jonathan.solar/)
+# [Seeing it speaks better than any readme can explain: Jonathan.Solar](https://jonathan.solar/)
 
 
 Jonathan.Solar started from a stupid joke about my name and eventually became my full-blown personal site. I'm using it to show off some of my technical-talents as well as a way for people to get to know more about me and check out some of my work :)
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,67 +1,75 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import githubLogo from '../Assets/Img/Header/github-logo.svg';
 
 export class NavBar extends React.Component {
   render () {
-    const style = {
-      wrapper: {
-        backgroundColor: '#262C35',
-        height: '100px',
-        boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
-        alignItems: 'center',
-        display: 'flex',
-        color: '#F6B427'
-      },
-
-      titleWrapper: {
-        fontSize: '40px',
-        alignItems: 'center',
-        display: 'flex'
-      },
-
-      image: {
-        display: 'inline',
-        margin: '15px'
-      },
-
-      titleText: {
-        display: 'inline'
-      },
-
-      navigationWrapper: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        width: '400px',
-        marginLeft: 'auto',
-        marginRight: '50px'
-      },
-
-      navigationItem: {
-        textDecoration: 'none',
-        color: '#F6B427'
-      }
-    };
-
     return (
-      <div style={style.wrapper}>
-        <div style={style.titleWrapper}>
+      <div className={css(styles.wrapper) + ' animated slideInDown'}>
+        <div className={css(styles.titleWrapper)}>
           <a href='https://github.com/Jspsun' target='_blank'>
-            <img style={style.image} src={githubLogo} alt='github' href='google.ca' />
+            <img className={css(styles.image)} src={githubLogo} alt='github' href='google.ca' />
           </a>
-          <div style={style.titleText}>
+          <div className={css(styles.titleText)}>
             Jonathan.Solar
           </div>
         </div>
 
-        <div style={style.navigationWrapper}>
-          <a href='#About Me' style={style.navigationItem}><div>About Me</div></a>
-          <a href='#Projects' style={style.navigationItem}><div>Projects</div></a>
-          <a href='#Contact' style={style.navigationItem}><div>Contact</div></a>
-          <a href='https://medium.com/@jspsun' target='_blank' style={style.navigationItem}><div>Blog</div></a>
-          <a href='./JonathanSunResume.pdf' target='_blank' style={style.navigationItem}><div>Resume</div></a>
+        <div className={css(styles.navigationWrapper)}>
+          <a href='#About Me' className={css(styles.navigationItem)}><div>About Me</div></a>
+          <a href='#Projects' className={css(styles.navigationItem)}><div>Projects</div></a>
+          <a href='#Contact' className={css(styles.navigationItem)}><div>Contact</div></a>
+          <a href='https://medium.com/@jspsun' target='_blank' className={css(styles.navigationItem)}><div>Blog</div></a>
+          <a href='./JonathanSunResume.pdf' target='_blank' className={css(styles.navigationItem)}><div>Resume</div></a>
         </div>
 
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#262C35',
+    height: '100px',
+    boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+    alignItems: 'center',
+    display: 'flex',
+    color: '#F6B427'
+  },
+
+  titleWrapper: {
+    fontSize: '40px',
+    alignItems: 'center',
+    display: 'flex'
+  },
+
+  image: {
+    display: 'inline',
+    margin: '15px',
+    ':hover': {
+      boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+      color: '#F6B427'
+    }
+  },
+
+  titleText: {
+    display: 'inline'
+  },
+
+  navigationWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+    marginLeft: 'auto',
+    marginRight: '50px',
+    '@media (max-width: 825px)': {
+      display: 'none'
+    }
+  },
+
+  navigationItem: {
+    textDecoration: 'none',
+    color: '#F6B427'
+  }
+});
