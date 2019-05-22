2d169ae48c28f3582338caa41b7655b4155c6f89
Jonathan Sun <Jspsun@gmail.com>
2019-05-22 00:51:24 -0700

    hacky deploy-ready

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { StyleSheet, css } from 'aphrodite';
+import { NavBar } from './NavBar.js';
+import {StarField} from './SolarSystem/StarField.js';
+import {SolarPanel} from './SolarPanel.js';
+import {AboutMe} from './AboutMe.js';
+import {Projects} from './Projects.js';
 
 import SimpleComponent from './SimpleComponent/SimpleComponent';
 
@@ -10,9 +16,25 @@ export default function Router() {
         <Route
           exact
           path="/"
-          render={() => <SimpleComponent message="it's a message" />}
+          render={
+            () => <div className={css(styles.wrapper)}>
+                    <StarField noOfStars='140' />
+                    <NavBar />
+                    <SolarPanel />
+                    <AboutMe />
+                    <Projects />
+                  </div>}
         />
       </Switch>
     </BrowserRouter>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    overflowY: 'visible',
+    overflowX: 'hidden',
+  },
+});
